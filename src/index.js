a46ff0c1de8b87882ca2ac804ebdad0a47cc34c2
nastik-e <105751995+nastik-e@users.noreply.github.com>
2022-07-12 10:09:19 +0300

    review

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
-exports.min = function min (array) {
-  return 0;
+
+
+exports.min = function min(array) {
+  let res = 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    // console.log(0);
+    return 0;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] < res) {
+      res = array[i];
+    }
+  }
+  return res;
+//   console.log(res);
 }
 
 exports.max = function max (array) {
-  return 0;
+  let res = 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    // console.log(0);
+    return 0;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] > res) {
+      res = array[i];
+    }
+  }
+  return res;
+//   console.log(res);
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  } 
+  let res = array.reduce((acc,el) => acc + el) / array.length;
+  return res;
+
 }
+
